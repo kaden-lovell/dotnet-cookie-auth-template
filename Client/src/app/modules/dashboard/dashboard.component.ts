fd5748a94ce5442973c4ad51c27bd635ba82de47
Kaden <kadenlovell@live.com>
2021-06-27 20:57:47 -0600

    included missed files from last commit

---
+++ Client/src/app/modules/dashboard/dashboard.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { BaseComponent } from '../base/base.component';
+import { UserService } from 'src/app/services/user/user.service';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  // styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent extends BaseComponent implements OnInit {
+  constructor(readonly _userService: UserService) {
+    super(_userService);
+  }
+
+  ngOnInit(): void {}
+}
